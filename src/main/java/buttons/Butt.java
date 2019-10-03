fbfcf482682be7ea6cda6446aad7589489f9c2cc
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-10-03 21:29:36 +0200

    Executable jar.

--- src/main/java/buttons/Butt.java
+++ src/main/java/buttons/Butt.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 
-import static com.sun.deploy.uitoolkit.ToolkitStore.dispose;
 
 public class Butt extends JButton implements ActionListener {
 
